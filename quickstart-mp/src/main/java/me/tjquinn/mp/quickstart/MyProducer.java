47b8be81f373d833f29b597525a7e54d342110de
tim.quinn@oracle.com <tim.quinn@oracle.com>
2023-08-10 11:35:23 -0500

    Check producer behavior

---
+++ quickstart-mp/src/main/java/me/tjquinn/mp/quickstart/MyProducer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.tjquinn.mp.quickstart;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.Produces;
+import jakarta.enterprise.inject.spi.InjectionPoint;
+
+@ApplicationScoped
+public class MyProducer {
+
+    @Produces
+    @MyQualifier
+    @Default
+    public MyBean createMyBeanWithQualifier(InjectionPoint ip) {
+        MyBean result = new MyBean();
+
+        MyQualifier myQ = ip.getAnnotated().getAnnotation(MyQualifier.class);
+        result.description(myQ == null ? "value-when-no-qualifier" : myQ.scope());
+        return result;
+    }
+}
